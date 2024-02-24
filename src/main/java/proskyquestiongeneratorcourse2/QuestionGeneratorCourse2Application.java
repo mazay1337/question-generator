5774d20f5300c26df98827c528fd43ff20ea2b62
Танин <mazayztn@yandex.ru>
2024-02-24 15:21:19 +0300

    initial commit

---
+++ src/main/java/proskyquestiongeneratorcourse2/QuestionGeneratorCourse2Application.java
@@ -0,0 +1,13 @@
+package proskyquestiongeneratorcourse2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionGeneratorCourse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionGeneratorCourse2Application.class, args);
+	}
+
+}
---
+++ src/test/java/proskyquestiongeneratorcourse2/QuestionGeneratorCourse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package proskyquestiongeneratorcourse2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGeneratorCourse2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
